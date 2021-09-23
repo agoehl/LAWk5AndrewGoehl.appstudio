04dc3e3bccf19b9bf25550073c833c4537791d97
Andrew Goehl <adg04939@creighton.edu>
2021-09-22 20:02:25 -0500

    combined multiple strings together to form a quote

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,3 +1,4 @@
+/*
 function Calculate(num1,num2) {
   let answer = num1 + num2
   return answer
@@ -8,3 +9,4 @@ let num2 = Number(prompt("Enter your second number:"))
 
 let sumNum = Calculate(num1,num2)
 console.log(`The sum of ${num1} and ${num2} is ${sumNum}`)
+*/
